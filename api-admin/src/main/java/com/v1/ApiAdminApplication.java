bf87a2a7db529286d57713f496da1705085ed786
sypatrick <booth20037@gmail.com>
2024-07-22 18:36:52 +0900

    initial commit

---
+++ api-admin/src/main/java/com/v1/ApiAdminApplication.java
@@ -0,0 +1,11 @@
+package com.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAdminApplication.class, args);
+    }
+}
