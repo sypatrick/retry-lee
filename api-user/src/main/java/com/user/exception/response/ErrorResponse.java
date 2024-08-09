2db8904e69eff9d7ac436dd2fc2979af2a4f2044
sypatrick <booth20037@gmail.com>
2024-08-09 11:16:34 +0900

    git issue

--- api-user/src/main/java/com/user/exception/response/ErrorResponse.java
+++ api-user/src/main/java/com/user/exception/response/ErrorResponse.java
@@ -3,18 +3,14 @@ package com.user.exception.response;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Map;
-
 @Getter
 @Setter
 public class ErrorResponse {
-    private int status;
     private String code;
     private String message;
-    private Map<String, String> errors;
 
-    public ErrorResponse(int status, String message) {
-        this.status = status;
+    public ErrorResponse(String code, String message) {
+        this.code = code;
         this.message = message;
     }
 }
